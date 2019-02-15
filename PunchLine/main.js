60ac94eaafa6e56cead592c832794a4eb306dd20
volysandro <volysandro@users.noreply.github.com>
2019-02-15 09:33:51 +0100

    vcvxcv

--- PunchLine/main.js
+++ PunchLine/main.js
@@ -1,1 +1,23 @@
 
+
+
+
+
+const btnabos = document.getElementById("trainingsabos");
+const btnplaene = document.getElementById("trainingsplan");
+
+
+
+btnplaene.addEventListener('click', e => {
+    document.getElementById("infoplaene").hidden = false;
+    document.getElementById("infoabos").hidden = true;
+
+
+})
+
+btnabos.addEventListener('click', e => {
+    document.getElementById("infoplaene").hidden = true;
+    document.getElementById("infoabos").hidden = false;
+
+
+})
