9e1393989f957c9cb640e9ce81095ab1c3d7cbab
Sandro Volery <sandro@volery.com>
2019-03-24 20:18:10 +0100

    big mess

--- BambinoHub/bambinohub/src/App.vue
+++ BambinoHub/bambinohub/src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <v-content>
+      <HelloWorld id="login"/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#login{
+  position: relative;
+  margin-top: 10px;
 }
 </style>
