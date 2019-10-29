31d3a467619252f0751b1a5948f4e367b552dbb2
Sandro Volery <sandro@volery.com>
2019-10-29 09:22:17 +0100

    update

---
+++ operationburgdorf/form.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <form action="form.php" method="post" enctype="multipart/form-data">
+      Name: <input type="text" name="name" value="">
+      <br>
+      Beschreibung: <input type="text" name="desc" value="">
+      <br>
+      Bild: <input type="file" name="fileToUpload" id="fileToUpload">
+      <br>
+      <br>
+      <input type="Submit" value="Eintragen">
+      <br>
+      <br>
+    </form>
+      <?php
+      session_start();
+      if(isset($_SESSION["verify"])AND password_verify($_SESSION["p"],$_SESSION["verify"])){
+
+      $path = "./pers";
+
+      if (isset($_POST["name"]) AND isset($_POST["desc"])) {
+
+        $name = $_POST["name"];
+        $desc = $_POST["desc"];
+
+      if ($name != "" AND $desc != "") {
+        if (!file_exists($path . "/" . $name)) {
+
+        //Ordner erstellen
+        mkdir("$path" . "/$name", 0700);
+
+        //Beschreibung
+        file_put_contents("$path/$name/desc.txt", $desc);
+
+        //Bild
+        $target_dir = $path . "/$name/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        //Bildprüfung
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                echo "Dateityp: " . $check["mime"];
+                echo "<br>";
+                $uploadOk = 1;
+            } else {
+                echo "Invalider Dateityp!";
+                echo "<br>";
+                $uploadOk = 0;
+              }
+            }
+            if ($_FILES["fileToUpload"]["size"] > 500000) {
+              echo "Zu grosse Datei!";
+              echo "<br>";
+              $uploadOk = 0;
+            }
+            if ($uploadOk == 0) {
+                echo "Fehler beim Upload!";
+                echo "<br>";
+            } else {
+              //Upload
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    echo basename( $_FILES["fileToUpload"]["name"]). " wurde hochgeladen!";
+                } else {
+                    echo "Unbekannter Fehler beim Upload!";
+                    echo "<br>";
+                }
+        }
+      }
+        //Doppeleintrag
+        else {
+          echo "Bereits Eingetragen!";
+          echo "<br>";
+        }
+      }
+        //Fehlerhaftes Formular
+        else {
+          echo "Unvollständige Angaben!";
+          echo "<br>";
+        }
+      }
+
+       ?>
+        <table style="width:100%">
+          <tr>
+            <th>Person</th>
+            <th>Beschreibung</th>
+            <th>Optionen</th>
+          </tr>
+          <?php
+          $path = "./pers";
+          $pers = preg_grep('/^([^.])/', scandir($path));
+          $entries = count($pers);
+
+          for ($i=2; $i <= $entries + 1; $i++) {
+            //shit kriegen
+            $object = scandir($path . "/" . $pers[$i]);
+            $desc = file_get_contents($path . "/" . $pers[$i] . "/" . $object[3]);
+            $value = array_search($pers[$i], $pers);
+            $img = $path . "/" . $pers[$i] . "/" . $object[2];
+            //tabelle
+            echo "
+            <tr>
+              <th>$pers[$i]</th>
+              <th>$desc</th>
+              <th><form action=\"form.php\" method=\"post\">
+                <input type=\"submit\" name=\"edit\" value=\"Löschen\" />
+                <input type=\"text\" name=\"del\" value=\"$value\" id=\"hidden\">
+                </form></th>
+            </tr>
+            ";
+          }
+          echo "</table>";
+          if (isset($_POST["del"])) {
+            //abfrage
+            $del = $pers[$_POST["del"]];
+            //alert
+            echo "        <div class=\"alert\">
+                      <h1>$del wirklich Löschen?</h1>
+                      <form class=\"confirm\" action=\"form.php\" method=\"get\">
+                        <input type=\"text\" name=\"del\" value=\"$del\" id=\"hidden\">
+                        <input type=\"Submit\" name=\"confirm\" value=\"Ja\">
+                        <input type=\"Submit\" name=\"confirm\" value=\"Nein\">
+                      </form>
+                    </div>";
+          }
+          if (isset($_GET["confirm"])) {
+            if ($_GET["confirm"] == "Ja") {
+              $del = $_GET["del"];
+              rename($path . "/$del", "./deleted/$del");
+              echo "<div class=\"alert\">
+                        <h1>$del wurde gelöscht!</h1>
+                        <form class=\"confirm\" action=\"form.php\" method=\"get\">
+                          <input type=\"Submit\" value=\"OK\">
+                        </form>";
+            }
+            else {
+              echo "<div class=\"alert\">
+                        <h1>Vorgang abgebrochen!</h1>
+                        <form class=\"confirm\" action=\"form.php\" method=\"get\">
+                          <input type=\"Submit\" value=\"OK\">
+                        </form>";
+            }
+          }
+        }
+        else{
+          header("Location: ./login/login.html");
+        }
+          ?>
+        </table>
+  </body>
+</html>
---
+++ operationburgdorf/team.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Team</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body>
+        <header>
+                <div id="navigation">
+        
+                    <div class="center" id="icons">
+                        <a href="index.html"><img class="icon" src="assets/icon_home.png" alt=""></a>
+                        <a href=""><img class="icon" src="assets/icon_user.png" alt=""></a>
+                        <a href="contact.html"><img class="icon" src="assets/icon_message.png" alt=""></a>
+                    </div>
+        
+                </div>
+        
+        
+        
+            </header>
+
+            <div class="center" id="titel">
+                <h1 id="title">Team und Unterstützung</h1>
+            </div>
+            <?php
+
+            $path = "./pers";
+            $pers = preg_grep('/^([^.])/', scandir($path));
+            $entries = count($pers);
+            $col = 0;
+
+            echo "<div class=\"row\">";
+        
+            for ($i=2; $i <= $entries + 1; $i++) {
+              //shit kriegen
+              $object = scandir($path . "/" . $pers[$i]);
+              $desc = file_get_contents($path . "/" . $pers[$i] . "/" . $object[3]);
+              $img = $path . "/" . $pers[$i] . "/" . $object[2];
+
+              if($col == 4){
+                $col = 0;
+                $col++;
+
+                echo "
+                </div>
+                <div style=\"margin-top: 80px;\" class=\"row\">
+
+                <div class=\"col s3 m3\">
+                  <div style=\"width: 350px; margin: 0 auto; height: 500px; border-radius:10px;\" class=\"person card hoverable\">
+                  <div style=\"height: 300px;\" class=\"card-image center\">
+                  <img style=\"height: 300px;\" src=\"$img\">
+                  </div>
+                  <h4>$pers[$i]</h4>
+                    <p>$desc</p>
+                  </div>
+                  </div>
+                ";
+              }
+              else{
+                $col++;
+
+                echo "
+                <div class=\"col s3 m3\">
+                  <div style=\"width: 350px; margin: 0 auto; height: 500px; border-radius:10px;\" class=\"person card hoverable\">
+                  <div style=\"height: 300px;\" class=\"card-image center\">
+                  <img style=\"height: 300px;\" src=\"$img\">
+                  </div>
+                  <h4>$pers[$i]</h4>
+                    <p>$desc</p>
+                  </div>
+                  </div>
+                ";
+              }
+              //ausgabe
+            }
+             ?>
+        
+            
+            <footer style="background-color: #f0f0f0;" class="page-footer">
+                <div style="color: #0082b4;" class="container">
+                  <div class="row">
+                    <div class="col l6 s12">
+                      <h5 class="">Credits</h5>
+                      <p class="">Erstellt mit viel Liebe von Schülern der Technischen Fachschule Bern.</p>
+                    </div>
+                    <div class="col l4 offset-l2 s12">
+                      <h5 class="">Links</h5>
+                      <ul>
+                        <li><a class="" href="team.html">Team</a></li>
+                        <li><a class="" href="contact.html">Kontakt</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div style="color: #000000;" class="footer-copyright">
+                  <div class="container">
+                  © 2019 Samis Moser, Liam Benedetti, Sandro Volery - Abteilung Informatik
+                  </div>
+                </div>
+              </footer>
+                
+</body>
+</html>
+
+<style>
+p{
+  font-size: 14px;
+}
+</style>
