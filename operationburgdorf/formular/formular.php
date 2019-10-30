293e0554a75225906b276dddb2aa77c841c374a6
Sandro Volery <sandro@volery.com>
2019-10-30 11:48:00 +0100

    update

--- operationburgdorf/formular/formular.php
+++ operationburgdorf/formular/formular.php
@@ -78,7 +78,7 @@ else{
   }
   else{
     # inputs OK
-    $to_mail = "sandro@volery.com";
+    $to_mail = "lukas.berger@tfbern.ch";
     $headers = "From: noreply@volery.com\nMIME-Version: 1.0\nContent-Type: text/html; charset=utf-8\n";
 
     $subject = "Eintrag Webseite Burgdorf, " . $name . " " . $lastname;
