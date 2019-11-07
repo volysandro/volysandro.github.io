fcc23e7b8e8acbeb63d43ce7267d40c7e1156fc3
Sandro Volery <sandro@volery.com>
2019-11-07 10:42:56 +0100

    update

--- bck/main.js
+++ bck/main.js
@@ -6,5 +6,13 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 
     document.getElementById("home-1").style.width = "90%"
     document.getElementById("home-2").style.width = "90%"
-    document.getElementById("logo").innerHTML = "<b>BC KERZERS</b>"
+    document.getElementById("logo").innerHTML = ""
+    document.getElementById("bcklogo").style.visibility = "visible"
+    document.getElementById("wallpaper").style.height = "100%"
+
+} else {
+    document.getElementById("bcklogo").style.position = "absolute"
+    document.getElementById("home-1").style.marginTop = "130px"
+    document.getElementById("wallpaper").style.width = "100%"
+
 }
